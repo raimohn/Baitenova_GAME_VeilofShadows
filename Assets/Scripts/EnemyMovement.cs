e55528385f3ef9f8a37744f2a918718012f3159d
Nurai <nuraimohn@gmail.com>
2025-03-08 21:36:33 +0500

    Enemy logic
    should add enemy spawn animation sprite - dark shadow sprite on the earth

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,61 +5,23 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [Header(" Elements ")]
-    [SerializeField] Player player;
-
-    [Header(" Spawn Sequence Related ")]
-    [SerializeField] private SpriteRenderer enemyRenderer;
-    [SerializeField] private SpriteRenderer spawnIndicator;
-    private bool hasSpawned;
+    private Player player;
 
     [Header(" Settings ")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-
-    [Header(" Debug ")]
-    [SerializeField] private bool gizmos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = FindFirstObjectByType<Player>();
-
-        if(player == null)
-        {
-            Debug.LogWarning("No player found");
-            Destroy(gameObject);
-        }
-
-        enemyRenderer.enabled = false;
-
-        spawnIndicator.enabled = true;
-        
-
-        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
-        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f)
-            .setLoopPingPong(4)
-            .setOnComplete(SpawnSequenceCompleted);
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(!hasSpawned)
-           return; 
-
-        FollowPlayer();
-        TryAttack();      
+        if(player != null)
+            FollowPlayer();
     }
 
-    private void SpawnSequenceCompleted()
+    public void StorePlayer(Player player)
     {
-        enemyRenderer.enabled = true;
-
-        spawnIndicator.enabled = false;
-
-        hasSpawned = true;
-
+        this.player = player;
     }
+
     private void FollowPlayer()
     {
         Vector2 direction = (player.transform.position - transform.position).normalized;
@@ -67,24 +29,5 @@ public class EnemyMovement : MonoBehaviour
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
-    }
-
-    private void TryAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-
-        if(distanceToPlayer <= playerDetectionRadius)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        if(!gizmos)
-            return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
